551b9a9566fbd14b5f71d56c5b9cccc564f9b1a9
jieyu <code@jieyu.ai>
2020-12-03 11:25:43 +0800

    🌈style: lint by black

--- jqadaptor/__init__.py
+++ jqadaptor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Aaron Yang"""
 __email__ = "code@jieyu.ai"
-__version__ = '0.2.6'
+__version__ = "0.2.7"
 
 from .fetcher import Fetcher
 
--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'arrow~=0.15', 
-    'ruamel.yaml~=0.16', 
-    'numpy==1.19.4', 
-    'jqdatasdk>=1.8',
-    'pytz>=2019.3', 
-    'zillionare-omicron>=0.1',
-    'cfg4py>=0.8.0',
-    'ruamel.yaml>=0.16'
+    "arrow~=0.15",
+    "ruamel.yaml~=0.16",
+    "numpy==1.19.4",
+    "jqdatasdk>=1.8",
+    "pytz>=2019.3",
+    "zillionare-omicron>=0.1",
+    "cfg4py>=0.8.0",
+    "ruamel.yaml>=0.16",
 ]
 
 setup_requirements = []
@@ -26,25 +26,25 @@ test_requirements = []
 
 setup(
     author="Aaron Yang",
-    author_email='code@jieyu.ai',
-    python_requires='>=3.8',
+    author_email="code@jieyu.ai",
+    python_requires=">=3.8",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Jqdatasdk adapter for zillionare omega",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='zillionare, omega, adaptors, jqdatasdk',
-    name='zillionare-omega-adaptors-jq',
-    packages=find_packages(include=['jqadaptor', 'jqadaptor.*']),
+    keywords="zillionare, omega, adaptors, jqdatasdk",
+    name="zillionare-omega-adaptors-jq",
+    packages=find_packages(include=["jqadaptor", "jqadaptor.*"]),
     setup_requires=setup_requirements,
-    url='https://github.com/zillionare/omega_jqadaptor',
-    version='0.2.6',
+    url="https://github.com/zillionare/omega_jqadaptor",
+    version="0.2.7",
     zip_safe=False,
 )
