7fb96c1671e2ee1900c53297d1361f621a2c146e
jieyu <code@jieyu.ai>
2020-12-04 15:17:37 +0800

    tets

--- jqadaptor/__init__.py
+++ jqadaptor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Aaron Yang"""
 __email__ = "code@jieyu.ai"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .fetcher import Fetcher
 
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     packages=find_packages(include=["jqadaptor", "jqadaptor.*"]),
     setup_requires=setup_requirements,
     url="https://github.com/zillionare/omega_jqadaptor",
-    version="0.3.0",
+    version="0.3.1",
     zip_safe=False,
 )
